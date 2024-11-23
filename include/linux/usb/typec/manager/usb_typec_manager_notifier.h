07a3155e3ce15f8a49fa942deedb2e1782eadcbb
Majaahh <maja.blaszczak@proton.me>
2024-11-23 20:31:33 -0300

    drivers: battery,mfd,muic,typec: Import drivers from a53x
    Change-Id: I6f332042bf8c6e72942a766111502a4d05f123e3
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- include/linux/usb/typec/manager/usb_typec_manager_notifier.h
+++ include/linux/usb/typec/manager/usb_typec_manager_notifier.h
@@ -148,14 +148,14 @@ typedef struct _manager_data_t
 	struct workqueue_struct *manager_muic_noti_wq;
 	struct manager_dwork usb_enum_check;
 	struct manager_dwork usb_event_by_vbus;
-#if IS_ENABLED(CONFIG_MUIC_POGO)
+#if IS_ENABLED(CONFIG_MUIC_POGO) || IS_ENABLED(CONFIG_MUIC_SM5504_POGO)
 	struct manager_dwork usb_event_by_pogo;
 #endif
 	struct manager_usb_dwork manager_usb_event_delayed_work;
 
 	struct mutex mo_lock;
 	int vbus_state;
-#if IS_ENABLED(CONFIG_MUIC_POGO)
+#if IS_ENABLED(CONFIG_MUIC_POGO) || IS_ENABLED(CONFIG_MUIC_SM5504_POGO)
 	int is_muic_pogo;
 #endif
 	int classified_cable_type;
