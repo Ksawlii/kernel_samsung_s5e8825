11bccfa7324caedf4b82535887ecd426dbc8ed30
Majaahh <maja.blaszczak@proton.me>
2024-11-23 20:31:33 -0300

    drivers: battery,mfd,muic,typec: Import drivers from a53x
    Change-Id: I6f332042bf8c6e72942a766111502a4d05f123e3
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- include/linux/muic/common/muic.h
+++ include/linux/muic/common/muic.h
@@ -678,7 +678,7 @@ enum power_supply_lsi_property {
 extern void muic_send_lcd_on_uevent(struct muic_platform_data *muic_pdata);
 extern int muic_set_hiccup_mode(int on_off);
 extern int muic_hv_charger_init(void);
-#if IS_ENABLED(CONFIG_MUIC_POGO)
+#if IS_ENABLED(CONFIG_MUIC_POGO) || IS_ENABLED(CONFIG_MUIC_SM5504_POGO)
 extern int muic_set_pogo_adc(int adc);
 #endif
 extern int muic_afc_get_voltage(void);
@@ -696,7 +696,7 @@ static inline void muic_send_lcd_on_uevent(struct muic_platform_data *muic_pdata
 static inline int muic_set_hiccup_mode(int on_off) {return 0; }
 static inline int muic_hv_charger_init(void) {return 0; }
 static inline int muic_afc_get_voltage(void) {return 0; }
-#if IS_ENABLED(CONFIG_MUIC_POGO)
+#if IS_ENABLED(CONFIG_MUIC_POGO) || IS_ENABLED(CONFIG_MUIC_SM5504_POGO)
 static inline int muic_set_pogo_adc(int adc) {return 0};
 #endif
 static inline int muic_afc_set_voltage(int voltage) {return 0; }
