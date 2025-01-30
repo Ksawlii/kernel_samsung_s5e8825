c364a6e505dd7d3b24ce0100efd5456cdf900cba
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-01-29 22:37:39 -0300

    input: sec_input: Always disable Dex touchpad support
    This causes a mouse pointer to show up on the lockscreen which will disappear when the screen is touched.
    
    * Based on https://github.com/crdroidandroid/android_kernel_samsung_exynos9820/commit/47320233b0588873e131d00d1f7103a4b8311a92
    
    Instead of disabling it in the dts, disable it in the driver itself.
    
    Change-Id: I3179f1d93639feb0c280a5952d71bce4f8450fa2
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- drivers/input/sec_input/sec_input.c
+++ drivers/input/sec_input/sec_input.c
@@ -1424,7 +1424,8 @@ void sec_input_support_feature_parse_dt(struct device *dev)
 
 	pdata->dev = dev;
 	pdata->regulator_boot_on = of_property_read_bool(np, "sec,regulator_boot_on");
-	pdata->support_dex = of_property_read_bool(np, "support_dex_mode");
+	// pdata->support_dex = of_property_read_bool(np, "support_dex_mode");
+	pdata->support_dex = false;
 	pdata->support_fod = of_property_read_bool(np, "support_fod");
 	pdata->support_fod_lp_mode = of_property_read_bool(np, "support_fod_lp_mode");
 	pdata->enable_settings_aot = of_property_read_bool(np, "enable_settings_aot");
